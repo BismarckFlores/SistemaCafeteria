7b6e0cbd5fab7198bab5f5d34321d1cdd8fa4ada
Bismarck Flores <bjflores@uamv.edu.ni>
2026-04-28 22:01:33 -0600

    Pedido.java terminado

--- src/modelos/Pedido.java
+++ src/modelos/Pedido.java
@@ -1,4 +1,34 @@
 package modelos;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Pedido {
+    private List<Producto> productos;
+    private String estado;
+
+    public Pedido() {
+        this.productos = new ArrayList<>();
+        this.estado = "Creado";
+    }
+
+    public void agregarProducto(Producto producto) {
+        this.productos.add(producto);
+    }
+
+    private void eliminarProducto(Producto producto) {
+        this.productos.remove(producto);
+    }
+
+    private void actualizarEstado(String nuevoEstado) {
+        this.estado = nuevoEstado;
+    }
+
+    public List<Producto> getProductos() {
+        return productos;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
 }
