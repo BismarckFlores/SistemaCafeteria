2a04ebadd7d70ab90ea4908279ac6d804747b966
Bismarck Flores <bjflores@uamv.edu.ni>
2026-04-28 22:04:06 -0600

    Se tiene los basico de los modelos, estos pueden llegar a ser cambiados a futuro

--- src/modelos/Producto.java
+++ src/modelos/Producto.java
@@ -4,8 +4,6 @@ public class Producto {
     private String nombre;
     private double precio;
 
-    public Producto() {}
-
     public Producto(String nombre, double precio) {
         this.nombre = nombre;
         this.precio = precio;
